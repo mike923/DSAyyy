89e4d6f001db2bbed9b3fbd179a1bd1c7ba17bc4
Jenesh <jenesh.napit@gmail.com>
2020-03-15 18:29:02 -0400

    Updating bestStriker solution

--- solutions/bestStrikers.js
+++ solutions/bestStrikers.js
@@ -4,8 +4,14 @@ const bestStrikers = (arr) => {
   let max = 0;
 
   for (let i = 0; i < arr.length; i++) {
-    
+    if (max < arr[i].goals) {
+      max = arr[i].goals
+    }
   }
+
+  arr.sort((a, b) => b.goals - a.goals)
+
+  console.log(max)
 
   return arr
 }
